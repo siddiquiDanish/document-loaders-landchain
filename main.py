4652ef924c6048dad9e1961687fdf7286bbc75ef
Danish Ahmed Siddiqui <dexterousdanish@gmail.com>
2024-09-03 15:34:20 +0530

    Initial commit for wikiloader

---
+++ main.py
@@ -0,0 +1,33 @@
+from langchain.prompts import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain_openai import ChatOpenAI
+from langchain.document_loaders import WikipediaLoader
+
+
+def answer_question_about(person_name, question):
+    # Get Wikipedia Article
+    docs = WikipediaLoader(query=person_name, load_max_docs=1)
+    context_text = docs.load()[0].page_content
+    print(context_text)
+    # Connect to OpenAI Model
+    f = open('C:\\Users\\dahmedsiddiqui\\Desktop\\OPEN_AI_KEY.txt')
+    api_key = f.read()
+    print(api_key)
+    model = ChatOpenAI(openai_api_key=api_key)
+
+    # Ask Model Question
+    human_prompt = HumanMessagePromptTemplate.from_template(
+        'Answer this question\n{question}, here is some extra context:\n{document}')
+
+    # Assemble chat prompt
+    chat_prompt = ChatPromptTemplate.from_messages([human_prompt])
+
+    # result
+    result = model(chat_prompt.format_prompt(question=question, document=context_text).to_messages())
+
+    print(result.content)
+
+answer_question_about("John F. Kennedy","When was he born?")
